385a3add4dc230d32e7b44cdc2965a1f806f2ac1
hyohyo814 <sojaisam@gmail.com>
2023-07-08 16:04:57 -0700

    Missing commit

--- src/store.js
+++ src/store.js
@@ -3,8 +3,6 @@ import anecdoteReducer, { setPosts } from './reducers/anecdoteReducer';
 import filterReducer from './reducers/filterReducer';
 import notifReducer from './reducers/notificationReducer'
 
-const _ = require('lodash')
-
 export const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
